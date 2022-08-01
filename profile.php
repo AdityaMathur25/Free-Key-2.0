2771005fed6f72498977979b7c60815f0c8170ef
Aditya Mathur <nishuproduction9@gmail.com>
2022-08-01 16:40:45 +0530

    Update profile.php

--- profile.php
+++ profile.php
@@ -6,183 +6,183 @@ require('database/db.php');
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="css/bootstrap.min.css" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" />
-    <link rel="icon" href="images/logo.ico">
-    <link rel="stylesheet" href="css/dataTables.bootstrap5.min.css" />
-    <link rel="stylesheet" href="css/style.css" />
-    <link rel="stylesheet" href="css/my.css" />
-    <title>FreeKey 2.0</title>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="css/bootstrap.min.css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" />
+  <link rel="icon" href="images/logo.ico">
+  <link rel="stylesheet" href="css/dataTables.bootstrap5.min.css" />
+  <link rel="stylesheet" href="css/style.css" />
+  <link rel="stylesheet" href="css/my.css" />
+  <title>FreeKey 2.0</title>
 </head>
 
-<body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-        <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar" aria-controls="offcanvasExample">
-                <span class="navbar-toggler-icon" data-bs-target="#sidebar"></span>
-            </button>
-            <a class="navbar-brand me-auto ms-lg-0 ms-3 text-uppercase fw-bold" href="#"> 🔑 FreeKey 2.0</a>
-            <div class="noisy">
-                <ul class="navbar-nav">
-                    <li class=" nav-item dropdown dropdown">
-                        <a class="nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown">
-                            <i class="bi bi-person-fill"></i>
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-end">
+<body style="background-color: #eee ;">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container-fluid">
+      <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar" aria-controls="offcanvasExample">
+        <span class="navbar-toggler-icon" data-bs-target="#sidebar"></span>
+      </button>
+      <a class="navbar-brand me-auto ms-lg-0 ms-3 text-uppercase fw-bold" href="#"> 🔑 FreeKey 2.0</a>
+      <div class="noisy">
+        <ul class="navbar-nav">
+          <li class=" nav-item dropdown dropdown">
+            <a class="nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown">
+              <i class="bi bi-person-fill"></i>
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end">
 
-                            <li><a class="dropdown-item" href="#">Profile Settings</a></li>
-                            <li>
-                                <a class="dropdown-item" href="#">Contact Support</a>
-                            </li>
-                            <li><a class="dropdown-item" style="border:3px red solid;" href="logout.php">Log Out</a></li>
-                            <li class="dropdown-item">
-                                <label for="border2">
-                                    <input checked="false" type="checkbox" id="border2" />
-                                    Default Mode
-                                </label>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-
-
-        </div>
-    </nav>
-    <!-- top navigation bar -->
-    <!-- offcanvas -->
-    <div class="offcanvas offcanvas-start sidebar-nav bg-dark" tabindex="-1" id="sidebar">
-        <div class="offcanvas-body p-0">
-            <nav class="navbar-dark">
-                <ul class="navbar-nav">
-                    <li>
-                        <a href="Dashboard.php" class="nav-link px-3 ">
-                            <span class="me-2"><i class="bi bi-columns-gap"></i></span>
-                            <span>Dashboard</span>
-                        </a>
-                    </li>
-                    <li class="my-1">
-                        <hr class="dropdown-divider bg-light" />
-                    </li>
-                    <li>
-                        <a href="application.php" class="nav-link px-3 ">
-                            <span class="me-2"><i class="bi bi-layers"></i></span>
-                            <span>Application</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="profile.php" class="nav-link px-3 active">
-                            <span class="me-2"><i class="bi bi-person-circle"></i></span>
-                            <span>Profile</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="Community.php" class="nav-link px-3 ">
-                            <span class="me-2"><i class="bi bi-people"></i></span>
-                            <span>Community</span>
-                        </a>
-                    </li>
-                    <li class="my-1">
-                        <hr class="dropdown-divider bg-light" />
-                    </li>
-                    <li>
-                        <div class="text-muted small fw-bold text-uppercase px-3 mb-3">
-                            Resources
-                        </div>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link px-3">
-                            <span class="me-2"><i class="bi bi-box-arrow-down"></i></span>
-                            <span>Downloads</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link px-3">
-                            <span class="me-2"><i class="bi bi-code-square"></i></span>
-                            <span>Documentation</span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </div>
-    <main class="mt-5 pt-3 px-2">
-    <section style="background-color: #eee;">
-  
-
-    <div class="row">
-      <div class="col-lg-4">
-        <div class="card mb-4">
-          <div class="card-body text-center">
-            <img src="images/ava3.webp" alt="avatar"
-              class="rounded-circle img-fluid" style="width: 150px;">
-            <h5 class="my-3"><?php echo $_SESSION['username']; ?></h5>
-            <p class="text-muted mb-1">Full Stack Developer</p>
-            <p class="text-muted mb-4">Bay Area, San Francisco, CA</p>
-            <div class="d-flex justify-content-center mb-2">
-              
-              <button type="button" class="btn btn-outline-primary ms-1 stretch">Social</button>
-            </div>
-          </div>
-        </div>
-        <div class="card mb-4 mb-lg-0">
-          <button type="button" class="btn btn-outline-primary">EDIT PROFILE</button>
-          
-        </div>
+              <li><a class="dropdown-item" href="#">Profile Settings</a></li>
+              <li>
+                <a class="dropdown-item" href="#">Contact Support</a>
+              </li>
+              <li><a class="dropdown-item" style="border:3px red solid;" href="logout.php">Log Out</a></li>
+              <li class="dropdown-item">
+                <label for="border2">
+                  <input checked="false" type="checkbox" id="border2" />
+                  Default Mode
+                </label>
+              </li>
+            </ul>
+          </li>
+        </ul>
       </div>
-      <div class="col-lg-8">
-        <div class="card mb-4">
-          <div class="card-body">
-            <div class="row">
-              <div class="col-sm-3">
-                <p class="mb-0">Full Name</p>
-              </div>
-              <div class="col-sm-9">
-                <p class="text-muted mb-0">Johnatan Smith</p>
-              </div>
+
+
+    </div>
+  </nav>
+  <!-- top navigation bar -->
+  <!-- offcanvas -->
+  <div class="offcanvas offcanvas-start sidebar-nav bg-dark" tabindex="-1" id="sidebar">
+    <div class="offcanvas-body p-0">
+      <nav class="navbar-dark">
+        <ul class="navbar-nav">
+          <li>
+            <a href="Dashboard.php" class="nav-link px-3 ">
+              <span class="me-2"><i class="bi bi-columns-gap"></i></span>
+              <span>Dashboard</span>
+            </a>
+          </li>
+          <li class="my-1">
+            <hr class="dropdown-divider bg-light" />
+          </li>
+          <li>
+            <a href="application.php" class="nav-link px-3 ">
+              <span class="me-2"><i class="bi bi-layers"></i></span>
+              <span>Application</span>
+            </a>
+          </li>
+          <li>
+            <a href="profile.php" class="nav-link px-3 active">
+              <span class="me-2"><i class="bi bi-person-circle"></i></span>
+              <span>Profile</span>
+            </a>
+          </li>
+          <li>
+            <a href="Community.php" class="nav-link px-3 ">
+              <span class="me-2"><i class="bi bi-people"></i></span>
+              <span>Community</span>
+            </a>
+          </li>
+          <li class="my-1">
+            <hr class="dropdown-divider bg-light" />
+          </li>
+          <li>
+            <div class="text-muted small fw-bold text-uppercase px-3 mb-3">
+              Resources
             </div>
-            <hr>
-            <div class="row">
-              <div class="col-sm-3">
-                <p class="mb-0">Email</p>
-              </div>
-              <div class="col-sm-9">
-                <p class="text-muted mb-0">example@example.com</p>
-              </div>
-            </div>
-            <hr>
-            <div class="row">
-              <div class="col-sm-3">
-                <p class="mb-0">Phone</p>
-              </div>
-              <div class="col-sm-9">
-                <p class="text-muted mb-0">(097) 234-5678</p>
-              </div>
-            </div>
-            <hr>
-            <div class="row">
-              <div class="col-sm-3">
-                <p class="mb-0">Mobile</p>
-              </div>
-              <div class="col-sm-9">
-                <p class="text-muted mb-0">(098) 765-4321</p>
-              </div>
-            </div>
-            <hr>
-            <div class="row">
-              <div class="col-sm-3">
-                <p class="mb-0">Address</p>
-              </div>
-              <div class="col-sm-9">
-                <p class="text-muted mb-0">Bay Area, San Francisco, CA</p>
+          </li>
+          <li>
+            <a href="#" class="nav-link px-3">
+              <span class="me-2"><i class="bi bi-box-arrow-down"></i></span>
+              <span>Downloads</span>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="nav-link px-3">
+              <span class="me-2"><i class="bi bi-code-square"></i></span>
+              <span>Documentation</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+  <main class="cop mt-5 pt-3 px-2">
+    <section style="background-color: #eee;">
+
+
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="card mb-4">
+            <div class="card-body text-center">
+              <img src="images/ava3.webp" alt="avatar" class="rounded-circle img-fluid" style="width: 150px;">
+              <h5 class="my-3"><?php echo $_SESSION['username']; ?></h5>
+              <p class="text-muted mb-1">Full Stack Developer</p>
+              <p class="text-muted mb-4">Bay Area, San Francisco, CA</p>
+              <div class="d-flex justify-content-center mb-2">
+
+                <button type="button" class="btn btn-outline-primary ms-1 stretch">Social</button>
               </div>
             </div>
           </div>
+
         </div>
-        <!-- <div class="row">
+        <div class="col-lg-8">
+          <div class="card mb-4">
+            <div class="card-body">
+              <div class="row">
+                <div class="col-sm-3">
+                  <p class="mb-0">Full Name</p>
+                </div>
+                <div class="col-sm-9">
+                  <p class="text-muted mb-0">Johnatan Smith</p>
+                </div>
+              </div>
+              <hr>
+              <div class="row">
+                <div class="col-sm-3">
+                  <p class="mb-0">Email</p>
+                </div>
+                <div class="col-sm-9">
+                  <p class="text-muted mb-0">example@example.com</p>
+                </div>
+              </div>
+              <hr>
+              <div class="row">
+                <div class="col-sm-3">
+                  <p class="mb-0">Phone</p>
+                </div>
+                <div class="col-sm-9">
+                  <p class="text-muted mb-0">(097) 234-5678</p>
+                </div>
+              </div>
+              <hr>
+              <div class="row">
+                <div class="col-sm-3">
+                  <p class="mb-0">Mobile</p>
+                </div>
+                <div class="col-sm-9">
+                  <p class="text-muted mb-0">(098) 765-4321</p>
+                </div>
+              </div>
+              <hr>
+              <div class="row">
+                <div class="col-sm-3">
+                  <p class="mb-0">Address</p>
+                </div>
+                <div class="col-sm-9">
+                  <p class="text-muted mb-0">Bay Area, San Francisco, CA</p>
+                </div>
+               
+              </div>
+            </div>
+          </div> <div class="cop card mb-4 mb-lg-0">
+                  <button type="button" class="btn btn-outline-primary " style="height: 60px;">EDIT PROFILE</button>
+
+                </div>
+          <!-- <div class="row">
           <div class="col-md-6">
             <div class="card mb-4 mb-md-0">
               <div class="card-body">
@@ -250,61 +250,61 @@ require('database/db.php');
             </div>
           </div>
         </div>-->
+        </div>
       </div>
-    </div> 
+      </div>
+    </section>
+
+  </main>
+
   </div>
-</section>
-
-    </main>
-
-    </div>
-    <footer class="footer ">
-        <!-- Grid container -->
-        <div class="container p-2 pb-0 ">
-            <!-- Section: Social media -->
-            <section class="mb-2">
-                <!-- Facebook -->
-                <a class="btn btn-primary btn-floating m-1
+  <footer class="footer ">
+    <!-- Grid container -->
+    <div class="container p-2 pb-0 ">
+      <!-- Section: Social media -->
+      <section class="mb-2">
+        <!-- Facebook -->
+        <a class="btn btn-primary btn-floating m-1
         href=" #!" role="button"><i class="bi bi-facebook"></i></a>
 
-                <!-- Twitter -->
-                <a class="btn btn-primary btn-floating m-1" style="background-color: #55acee;" href="#!" role="button"><i class="bi bi-twitter"></i></a>
+        <!-- Twitter -->
+        <a class="btn btn-primary btn-floating m-1" style="background-color: #55acee;" href="#!" role="button"><i class="bi bi-twitter"></i></a>
 
-                <!-- Google -->
-                <a class="btn btn-primary btn-floating m-1" style="background-color: #dd4b39;" href="#!" role="button"><i class="bi bi-youtube"></i></a>
+        <!-- Google -->
+        <a class="btn btn-primary btn-floating m-1" style="background-color: #dd4b39;" href="#!" role="button"><i class="bi bi-youtube"></i></a>
 
-                <!-- Instagram -->
-                <a class="btn btn-primary btn-floating m-1" style="background-color: #ac2bac;" href="#!" role="button"><i class="bi bi-instagram"></i></a>
+        <!-- Instagram -->
+        <a class="btn btn-primary btn-floating m-1" style="background-color: #ac2bac;" href="#!" role="button"><i class="bi bi-instagram"></i></a>
 
-                <!-- Linkedin -->
-                <a class="btn btn-primary btn-floating m-1" style="background-color: #0082ca;" href="#!" role="button"><i class="bi bi-linkedin"></i></a>
-                <!-- Github -->
-                <a class="btn btn-primary btn-floating m-1" style="background-color: #333333;" href="#!" role="button"><i class="bi bi-github"></i></a>
-            </section>
-            <!-- Section: Social media -->
-        </div>
-        <!-- Grid container -->
-
-        <!-- Copyright -->
-        <div class="leftt text-center " style="background-color: rgb(33, 37, 41);">
-            © 2022 Copyright:
-            <a class="text-white" href="#" style="text-decoration: none;">🔑 FREEKEY 2.0</a>
-        </div>
-        <!-- Copyright -->
-    </footer>
+        <!-- Linkedin -->
+        <a class="btn btn-primary btn-floating m-1" style="background-color: #0082ca;" href="#!" role="button"><i class="bi bi-linkedin"></i></a>
+        <!-- Github -->
+        <a class="btn btn-primary btn-floating m-1" style="background-color: #333333;" href="#!" role="button"><i class="bi bi-github"></i></a>
+      </section>
+      <!-- Section: Social media -->
     </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="leftt text-center " style="background-color: rgb(33, 37, 41);">
+      © 2022 Copyright:
+      <a class="text-white" href="#" style="text-decoration: none;">🔑 FREEKEY 2.0</a>
     </div>
-    </div>
-    </div>
+    <!-- Copyright -->
+  </footer>
+  </div>
+  </div>
+  </div>
+  </div>
 
 
-    <script src="./js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.0.2/dist/chart.min.js"></script>
-    <script src="./js/jquery-3.5.1.js"></script>
-    <script src="./js/jquery.dataTables.min.js"></script>
-    <script src="./js/dataTables.bootstrap5.min.js"></script>
-    <script src="./js/script.js"></script>
-    <script src="./js/my.js"></script>
+  <script src="./js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/chart.js@3.0.2/dist/chart.min.js"></script>
+  <script src="./js/jquery-3.5.1.js"></script>
+  <script src="./js/jquery.dataTables.min.js"></script>
+  <script src="./js/dataTables.bootstrap5.min.js"></script>
+  <script src="./js/script.js"></script>
+  <script src="./js/my.js"></script>
 
 </body>
 
