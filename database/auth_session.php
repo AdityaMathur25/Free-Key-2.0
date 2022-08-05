96163c86dbd2a72a303edfb28289d00a0c5ba51f
Aditya Mathur <nishuproduction9@gmail.com>
2022-08-05 15:08:16 +0530

    update

--- database/auth_session.php
+++ database/auth_session.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     if(!isset($_SESSION["username"])) {
-        header("Location: login.php");
+        header("Location: /Free-Key-2.0/login.php");
         exit();
     }
    // $_SESSION["username"] ?? " " ;
--- not-found.php
+++ not-found.php
@@ -28,42 +28,6 @@ require('database/db.php');
             <p align="center"><button type="button" class="button4 btn btn-primary" onclick="location.href = '/Free-Key-2.0/dashboard.php';">Go Back</button></p>
 
         </div>
-
-    <footer class="footer bg-light text-center text-white">
-        <!-- Grid container -->
-        <div class="container p-4 pb-0">
-            <!-- Section: Social media -->
-            <section class="mb-4">
-                <!-- Facebook -->
-                <a class="btn btn-primary btn-floating m-1
-        href=" #!"="" role="button"><i class="bi bi-facebook"></i></a>
-
-                <!-- Twitter -->
-                <a class="btn btn-primary btn-floating m-1" style="background-color: #55acee;" href="#!" role="button"><i class="bi bi-twitter"></i></a>
-
-                <!-- Google -->
-                <a class="btn btn-primary btn-floating m-1" style="background-color: #dd4b39;" href="#!" role="button"><i class="bi bi-youtube"></i></a>
-
-                <!-- Instagram -->
-                <a class="btn btn-primary btn-floating m-1" style="background-color: #ac2bac;" href="#!" role="button"><i class="bi bi-instagram"></i></a>
-
-                <!-- Linkedin -->
-                <a class="btn btn-primary btn-floating m-1" style="background-color: #0082ca;" href="#!" role="button"><i class="bi bi-linkedin"></i></a>
-                <!-- Github -->
-                <a class="btn btn-primary btn-floating m-1" style="background-color: #333333;" href="#!" role="button"><i class="bi bi-github"></i></a>
-            </section>
-            <!-- Section: Social media -->
-        </div>
-        <!-- Grid container -->
-
-        <!-- Copyright -->
-        <div class="text-center p-3" style="background-color: rgb(33, 37, 41);">
-            © 2022 Copyright:
-            <a class="text-white" href="#" style="text-decoration: none;">🔑 FREEKEY 2.0</a>
-            <!-- </div> -->
-            <!-- Copyright -->
-        </div>
-    </footer>
     </div>
     </div>
     </div>
