9e95cb031137fd7dca8514d7a71599d209269cbe
Aditya Mathur <nishuproduction9@gmail.com>
2022-08-07 16:07:43 +0530

    update

--- function/create-app.php
+++ function/create-app.php
@@ -114,43 +114,56 @@ $user = $_SESSION['username'];
     </div>
 
     <main class="cop mt-2 pt-3 px-2">
-        <div class="form-body ">
-            <div class="row">
-                <div class="form-holder">
-                    <div class="form-content">
-                        <div class="form-items">
-                            <?php
-                            error_reporting(E_ERROR | E_PARSE);
-                            if ($_POST["name"] || $_POST["age"]) {
-                                if (preg_match("/[^A-Za-z'-]/", $_POST['name'])) {
-                                    die("invalid name and name should be alpha");
-                                }
-                                echo "Welcome " . $_POST['name'] . "<br />";
-                                echo "You are " . $_POST['age'] . " years old.";
+    <?php
+// define variables and set to empty values
+$name = $email = $gender = $comment = $website = "";
 
-                                exit();
-                            }
-                            ?>
-                            <h3>Create App</h3>
-                            <p>Fill in the data below to create a great app .</p>
-                            <form class="requires-validation" action="<?php $_PHP_SELF ?>" method="POST" novalidate>
-                                <div class="col-md-12">
-                                    <input class="alc form-control" type="text" name="aname" placeholder="ENTER APPLICATION NAME" required>
-                                    <p class="text-muted" style="text-align: left ; margin-bottom: 0px;">Create a legit name else your app will be removed by admin</p>
-                                </div>
-                                
-                                <div class="col-md-12">
-                                    <input class="form-control" type="text" name="age" value="<?php echo $data['email'] ?>" required>
-                                </div>
-                                <div class="form-button mt-3">
-                                    <button id="submit" type="submit" class="btn btn-primary stretch" style="width: 100%;">Create App</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = test_input($_POST["name"]);
+  $email = test_input($_POST["email"]);
+  $website = test_input($_POST["website"]);
+  $comment = test_input($_POST["comment"]);
+  $gender = test_input($_POST["gender"]);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+<h2>PHP Form Validation Example</h2>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name">
+  <br><br>
+  E-mail: <input type="text" name="email">
+  <br><br>
+  Website: <input type="text" name="website">
+  <br><br>
+  Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" value="female">Female
+  <input type="radio" name="gender" value="male">Male
+  <input type="radio" name="gender" value="other">Other
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
 
     </main>
 
