14e915d3bec6c3acdee6aedf70501fbd6f9f2d6b
Aditya Mathur <61025539+AdityaMathur25@users.noreply.github.com>
2022-08-28 00:56:49 +0530

    update

--- database/auth_session.php
+++ database/auth_session.php
@@ -1,5 +1,7 @@
 <?php
     session_start();
+    //$username = $_POST['username'];
+    //$_SESSION['username'] = $username;
     if(!isset($_SESSION["username"])) {
         header("Location: /Free-Key-2.0/login.php");
         exit();
